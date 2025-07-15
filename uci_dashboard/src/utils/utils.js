9521dda0297ede1566005e759564440a81cbe133
Sagar Sharma <123538728+Amansagar1@users.noreply.github.com>
2025-07-15 15:25:31 +0530

    Delete uci_dashboard directory

--- uci_dashboard/src/utils/utils.js
+++
@@ -1,6 +0,0 @@
-// src/utils.js
-export const getRobotName = (sectionTitle, index) => {
-    const prefix = sectionTitle === "GRA" ? "R" : sectionTitle === "GCS" ? "R" : "P";
-    return `${prefix}${index + 1}`;
-  };
-  
